274b32938d94db684f7e9f41d64897f6ba3308e3
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-07-28 11:42:42 +0000

    node js file systems

---
+++ Notes.md
@@ -0,0 +1,5 @@
+*Node js is not a -programming language
+                  -a framework
+                  -a library
+
+*it is a javascript runtime environment
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs=require('fs')
+
+// to create a new file in same directory
+// fs.writeFile("hey.txt","new file created",(err)=>{
+//     if(err) console.log(err);
+//     else console.log("file created");
+// })
+
+// to append a text to an existing file
+// fs.appendFile("hey.txt"," , append text",(err)=>{
+//     if(err) console.log(err.message);
+//     else console.log("Appended");
+// })
+
+//to delete a file
+// fs.unlink("hey.txt",(err)=>{
+//     if(err) console.log(err.message);
+//     else console.log("file removed");
+// })
+
+// to delete a folder , if folder is empty keep recursive true else false
+// fs.rm('./copy',{recursive:true},(err)=>{
+//     if(err) console.log(err.message);
+//     else console.log("folder deleted");
+// })
+
+//to read a file
+fs.readFile('Notes.md',(err,data)=>{
+    if(err) console.log(err.message);
+    console.log("file read" ,data.toLocaleString());
+})
