2c3f15138bcc17bf08d1b1ba44686e35cef1d3a8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-08-02 14:33:09 +0000

    handling routes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express
+const express=require('express')
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("home page")
+})
+app.get('/profiles',(req,res)=>{
+    res.send("profiles page");
+})
+app.get('/*',(req,res)=>{
+    res.send("404 not found")
+})
+app.listen(3000);
