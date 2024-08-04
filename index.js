1c1c44b8a0aaaee9b92ba125fa975f3b9e3bf925
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-08-04 04:16:56 +0000

    handling forms

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express=require('express')
 const app=express();
+app.set("view engine","ejs")
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
-    res.send("home page")
+    res.render('form')
 })
 app.get('/profiles',(req,res)=>{
     res.send("profiles page");
 })
-app.get('/*',(req,res)=>{
-    res.send("404 not found")
+// app.get('/*',(req,res)=>{
+//     res.send("404 not found")
+// })
+app.post('/submitdata',(req,res)=>{
+    console.log(req.query);
+    res.send("data saved")
+    
 })
 app.listen(3000);
