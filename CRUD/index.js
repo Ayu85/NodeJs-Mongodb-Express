7eaa23c43d891746b3845b10807548c6832a17c1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-08-08 10:24:09 +0000

    sending json in response

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const server = express();
+const fs=require('fs')
+const users=fs.readFileSync('./mock_data/users.json')
+server.get('/users',(req,res)=>{
+    res.setHeader('Content-Type','application/json')
+    res.send(users);
+})
 
 
-
-
-
-
-
-
-server.listen(3000,(req,res)=>{
+server.listen(3000,()=>{
     console.log("Server Started");
     
 })
